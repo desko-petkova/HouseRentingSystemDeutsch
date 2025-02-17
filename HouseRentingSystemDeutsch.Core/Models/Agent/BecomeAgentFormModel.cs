e95dd52f41c5af1f37333ca6f6d5221e8799baf4
desko-petkova <desko.petkova@gmail.com>
2025-02-17 12:32:46 +0200

    IAgentService and AgentService added

---
+++ HouseRentingSystemDeutsch.Core/Models/Agent/BecomeAgentFormModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystemDeutsch.Infrastructure.Constants.DataConstants;
+
+namespace HouseRentingSystemDeutsch.Core.Models.Agent
+{
+    public class BecomeAgentFormModel
+    {
+        [Required]
+        [MaxLength(PhoneNumberMaxLength), MinLength(PhoneNumberMinLength)]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+    }
+}
