14527b9090c4798a12664957b8ca0cf769d09c15
desko-petkova <desko.petkova@gmail.com>
2025-02-17 12:33:13 +0200

    controller and view for become agent added

--- HouseRentingSystemDeutsch/Program.cs
+++ HouseRentingSystemDeutsch/Program.cs
@@ -1,6 +1,8 @@
 using HouseRentingSystemDeutch.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using HouseRentingSystemDeutsch.Core.Contracts;
+using HouseRentingSystemDeutsch.Core.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,8 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
-	
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddScoped<IAgentService, AgentService>();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 {
